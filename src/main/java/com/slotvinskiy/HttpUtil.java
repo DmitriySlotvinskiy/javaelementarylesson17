80475935373321df2afb681914b486dd4ffa4282
Dmitriy Slotvinskiy <slotvinskiy@gamil.com>
2020-03-28 15:17:00 +0200

    Initial commit

---
+++ src/main/java/com/slotvinskiy/HttpUtil.java
@@ -0,0 +1,48 @@
+package com.slotvinskiy;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpUtil {
+
+    public static Response sendRequest(String url) {
+        Response response = new Response();
+
+        HttpURLConnection urlConnection = null;
+        try {
+            URL requestUrl = new URL(url);
+            urlConnection = (HttpURLConnection) requestUrl.openConnection();
+            urlConnection.setReadTimeout(20000);
+            urlConnection.setConnectTimeout(20000);
+            urlConnection.setRequestMethod("GET"); // optional, GET already by default
+
+            int status = urlConnection.getResponseCode();
+            response.setResponseCode(status);
+            if (status == HttpURLConnection.HTTP_OK) {
+                response.setBody(getStringFromStream(urlConnection.getInputStream()));
+            }
+        } catch (Exception e) {
+            response.setException(e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+        return response;
+    }
+
+    private static String getStringFromStream(InputStream inputStream) throws IOException {
+        final int BUFFER_SIZE = 4096;
+        ByteArrayOutputStream resultStream = new ByteArrayOutputStream(BUFFER_SIZE);
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int length;
+        while ((length = inputStream.read(buffer)) != -1) {
+            resultStream.write(buffer, 0, length);
+        }
+        return resultStream.toString("UTF-8");
+    }
+
+}
---
+++ src/main/java/com/slotvinskiy/Task1.java
@@ -0,0 +1,59 @@
+package com.slotvinskiy;
+
+import com.google.gson.Gson;
+
+import java.net.HttpURLConnection;
+import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+//1) Написать консольную программу которая спрашивает у пользователя дату в формате 25.03.2020;
+// И выводит курс доллара за эту дату или описание ошибки, используя HttpURLConnection.
+//https://api.privatbank.ua/p24api/exchange_rates?json&date=01.12.2014
+
+public class Task1 {
+
+    public static final LocalDate CURRENT_DATE = LocalDate.now();
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("d.M.yyyy");
+    public static final LocalDate PB_ESTABLISH_DATE = LocalDate.parse("19.03.1992", FORMATTER);
+    public static final String URL = "https://api.privatbank.ua/p24api/exchange_rates?json&date=";
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter date in format day.month.year: ");
+        String date;
+        do {
+            date = scanner.nextLine();
+        } while (!isCorrect(date));
+
+        Response response = HttpUtil.sendRequest(URL + date);
+        if (response.getResponseCode() == HttpURLConnection.HTTP_OK) {
+            Gson gson = new Gson();
+            ClassForParseResponseJson responseJson = gson.fromJson(response.getBody(), ClassForParseResponseJson.class);
+            System.out.println(responseJson.get("USD"));
+        } else {
+            System.out.println("Something went wrong. Status code " + response.getResponseCode());
+        }
+    }
+
+    private static boolean isCorrect(String date) {
+        if (date == null || date.equals("") || date.length() < 8) {
+            showErrorMessage();
+            return false;
+        }
+        LocalDate inputDate = LocalDate.parse(date, FORMATTER);
+        if (inputDate.isAfter(ChronoLocalDate.from(PB_ESTABLISH_DATE)) &&
+                inputDate.isBefore(ChronoLocalDate.from(CURRENT_DATE))) {
+            return true;
+        } else {
+            showErrorMessage();
+            return false;
+        }
+    }
+
+    private static void showErrorMessage() {
+        System.out.println("Wrong data format! Try to enter date one more time. Format day.month.year: ");
+    }
+}
